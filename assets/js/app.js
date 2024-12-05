af3a6ed6b086ae00d58782799a30d5334bfacd99
AhmedNabil1994 <94ahmed.nabil@gmail.com>
2024-12-05 16:10:16 +0200

    signup validation and redirect to login page

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,20 +14,38 @@ let users = [];
 console.log(users, "after get from localstorage");
 
 function createUser() {
-  let user = {
-    name: usernameInput.value,
-    email: userEmailInput.value,
-    password: userPasswordInput.value,
-  };
-  users.push(user);
-  // console.log(users);
-  setToLocalStorage("users", users);
-  clearForm();
+  if (
+    validateForm(usernameInput) &&
+    validateForm(userEmailInput) &&
+    validateForm(userPasswordInput)
+  ) {
+    let user = {
+      name: usernameInput.value,
+      email: userEmailInput.value,
+      password: userPasswordInput.value,
+    };
+    users.push(user);
+    // console.log(users);
+    setToLocalStorage("users", users);
+    // clearForm();
+    window.location.href = "../../index.html";
+  }
+  if (
+    usernameInput.value.length === 0 &&
+    userEmailInput.value.length === 0 &&
+    userPasswordInput.value.length === 0
+  ) {
+    clearForm();
+    document.querySelector("p.error-msg").classList.remove("d-none");
+  } else {
+    document.querySelector("p.error-msg").classList.add("d-none");
+  }
 }
-
-signupBtn.addEventListener("click", function () {
-  createUser();
-});
+if (signupBtn) {
+  signupBtn.addEventListener("click", function () {
+    createUser();
+  });
+}
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -37,6 +55,9 @@ function clearForm() {
   usernameInput.value = null;
   userEmailInput.value = null;
   userPasswordInput.value = null;
+  usernameInput.nextElementSibling.classList.add("d-none");
+  userEmailInput.nextElementSibling.classList.add("d-none");
+  userPasswordInput.nextElementSibling.classList.add("d-none");
 }
 
 function setToLocalStorage(key, val) {
@@ -46,3 +67,28 @@ function setToLocalStorage(key, val) {
 function getFromLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
+
+function validateForm(input) {
+  let regex = {
+    name: /^[a-zA-z]{3,15}$/,
+    email: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
+    password: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/,
+  };
+  let isValid = regex[input.id].test(input.value);
+  if (isValid) {
+    input.nextElementSibling.classList.replace("d-block", "d-none");
+  } else {
+    input.nextElementSibling.classList.replace("d-none", "d-block");
+  }
+  return isValid;
+}
+
+function handleInputClick() {
+  let inputs = Array.from(document.querySelectorAll("input"));
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener("input", function (e) {
+      validateForm(e.target);
+    });
+  }
+}
+handleInputClick();
